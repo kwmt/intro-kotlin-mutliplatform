4632c52ea2c8b6a7d9bd14f48f4568d5dd402dea
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-10-04 00:30:27 +0900

    kotlin/jsのindex.htmlを作成

---
+++ src/jsMain/kotlin/com/jetbrains/handson/introMpp/main.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.handson.introMpp
+
+import kotlinx.html.dom.append
+import kotlinx.html.js.h1
+import kotlinx.html.js.h2
+import kotlinx.html.js.img
+import kotlin.browser.document
+
+const val jvmBackend = "http://127.0.0.1:8888"
+
+fun main() {
+    document.getElementById("app")
+            ?.also { it.innerHTML = "" }
+            ?.append {
+                h1 { +"Kotlin Fractals" }
+                h2 { +"JS Edition" }
+                img(src = "$jvmBackend/mandelbrot")
+            }
+}
