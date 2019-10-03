98266c1156ae8810823795393e88d2f3129bfc99
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-10-04 01:04:51 +0900

    share code

--- src/commonMain/kotlin/com/jetbrains/handson/introMpp/common.kt
+++ src/commonMain/kotlin/com/jetbrains/handson/introMpp/common.kt
@@ -2,3 +2,8 @@ package com.jetbrains.handson.introMpp
 
 const val jvmHost = "127.0.0.1"
 const val jvmPort = 8888
+
+object Colors
+expect class Color
+expect fun Colors.newColor(r: Int, g: Int, b: Int): Color
+expect val Colors.BLACK :Color
